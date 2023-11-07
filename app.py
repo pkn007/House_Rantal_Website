a1a3e3a727a5c4f63aef11ef904a011ac675b468
Pritam S Gurav <106548018+Pritam1729@users.noreply.github.com>
2023-11-07 11:19:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import mysql.connector
 
 app = Flask(__name__)
 
-
 db=mysql.connector.connect(
    host="127.0.0.1",
    port="3306",
@@ -17,11 +16,10 @@ db=mysql.connector.connect(
 
 cur = db.cursor()
 
-cur.execute("SELECT * FROM tenant")
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('home.html') 
 
 
 @app.route('/user_login', methods = ['GET','POST'])
@@ -37,7 +35,7 @@ def login():
             
         else:
             msg = 'Incorrect username/password'
-    return render_template('login.html',msg = msg)
+    return render_template('User_login.html',msg = msg)
 
 @app.route('/user_register',methods = ['GET','POST'])
 def register():
@@ -76,6 +74,7 @@ def owner_login():
         accounts = cur.fetchone()
         if accounts:
             msg = "Land_Lord Logged In"
+            return redirect(url_for('owner',msg = accounts[1]))
         else:
             msg = "Enter Correct Username or Password"
     return render_template('owner_login.html',msg = msg)
@@ -96,19 +95,45 @@ def owner_register():
             cur.execute("insert into land_lord (username,password,name,contact_info) values (%s,%s,%s,%s)",(ownername,password,name,contact))
             db.commit()
             msg = "Land_Lord Register"
+            return redirect('owner',msg = ownername)
     return render_template('owner_register.html',msg = msg)
         
         
-@app.route('/owner/house_register',methods = ['GET','POST'])
+@app.route('/house_register',methods = ['GET','POST'])
 def house_register():
-    msg = ''
-    if request.method == 'POST':
-        rent = request.form("rent")
+    msg = ''  
+    ok = request.args.get('id')
+    if ok:
+        id = ok
+    # if request.method == 'POST':
+    if request.method == "POST":
+        size = request.form['size']
+        size = request.form['size']
+        size = request.form['size']
+        size = request.form['size']
+        
+    return render_template('house.html',msg = msg)
+        
+@app.route('/owner')
+def owner():
+    msg = []
+    name = request.args.get('msg')
+    account = None
+    if id:
+        cur.execute(f"SELECT * FROM land_lord where username = '{name}'")
+        account = cur.fetchone()
+    return render_template('owner_page.html',name = account[1],id = account[0])
         
     
 if __name__ == '__main__':
     app.run(debug = True)       
 
 
+
+
+# eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByaXRhbWd1cmF2OTUyNzJAZ21haWwuY29tIiwiZ29vZ2xlQW5hbHl0aWNzSWQiOiIxNjc2Mjc3ODc2LjE2OTkyNTA4NTgiLCJtaXhwYW5lbElkIjoiJGRldmljZToxOGJhMzNlNWViMzFmOTUtMDc5ZWE2YmVkNjNmNzMtMjYwMzExNTEtMTQ0MDAwLTE4YmEzM2U1ZWIzMWY5NSIsIm1peHBhbmVsUHJvamVjdElkIjoiNGJmYjI0MTRhYjk3M2M3NDFiNmYwNjdiZjA2ZDU1NzUiLCJvcmciOiJwZXMgdW5pdmVyc2l0eSIsInB1YiI6Im5lbzRqLmNvbSIsInJlZyI6InByaXRhbSBndXJhdiIsInN1YiI6Im5lbzRqLWRlc2t0b3AiLCJleHAiOjE3MzA4NzMzMDgsInZlciI6IioiLCJpc3MiOiJuZW80ai5jb20iLCJuYmYiOjE2OTkyNTA5MDgsImlhdCI6MTY5OTI1MDkwOCwianRpIjoiNm1LdVZVRjEyIn0.rzldcTBEQJ7NjoT3WPGoBkE_HyiMHp3QzB8zXgwRjhSTDOyoaLLLkAYB8SZqvFiaQv3UdsyWbE7v6lGQZoEd9nV7EV5hlU1WjRhr3QhL7Io3hZeQjWeRblOisH4vKQW7_FxI2whQNcqOZj7aS9uJvbjX871vQEdmKXjsFge47-XusqrFyp8R96uqBGc0hALuJvffHKQPfWAstesO640BcCj8YlApjmsTA1eg6N5e4ZWWo7rM2mL__IRt9ejSTQ6EwhKXqQQiEdqF63x4US1n3Y2udt2h0okESWjd57HZBG9iWNJq8LsjLD73xNaO7b_Useyh4eRfp-20KapQ5mb2SQ
+
     
 
+
+# 118000878027911
